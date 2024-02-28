89e4e757e3ec0787cadbab860d33da1c4e763e59
sabb12 <sabb0923@gmail.com>
2024-02-28 17:09:32 +0900

    chore: 초기 폴더 구조 세팅

---
+++ models/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.json")[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
