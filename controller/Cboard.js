a5859e746045d51ac01fb3127197f5af331f6445
gusnl88 <gusnl88@naver.com>
2024-03-04 15:49:29 +0900

    feat,fix: 게시판기능추가 수정

--- controller/Cboard.js
+++ controller/Cboard.js
@@ -1,33 +1,91 @@
 const { board, boardLike, comment } = require("../models");
 const sequelize = require("sequelize");
 exports.boardList = async (req, res) => {
+    console.log('진입')
+    console.log(req.query)
     const page = req.query.page || 1; // 요청된 페이지 번호, 기본값은 1
-    const pageSize = 5; // 한 페이지에 표시할 항목의 수
+    const category = req.query.category;
+    const like = req.query.like;
+    console.log(like,category)
+    const pageSize = 2; // 한 페이지에 표시할 항목의 수
     try {
         const totalCount = await board.count(); // 전체 데이터의 수
         const totalPages = Math.ceil(totalCount / pageSize); // 전체 페이지 수
         const offset = (page - 1) * pageSize; // 오프셋 계산
-        const boardList = await board.findAll({
-            limit: pageSize,
-            offset: offset,
-            include: [
-                {
-                    model: boardLike,
-                    attributes: [],
-                },
-            ],
-            attributes: {
+        if (!category) {
+            let orderby = [];
+            if (like) {
+                orderby.push("like_count");
+            } else {
+                orderby.push("b_id");
+            }
+            const boardList = await board.findAll({
+                limit: pageSize,
+                offset: offset,
                 include: [
-                    [
-                        sequelize.literal(
-                            "(SELECT COUNT(*) FROM `board_like` WHERE `board_like`.`b_id` = `board`.`b_id`)",
-                        ),
-                        "like_count",
-                    ],
+                    {
+                        model: boardLike,
+                        attributes: [],
+                    },
                 ],
-            },
-        });
-        res.render("board/main", { boardList: boardList, totalPages: totalPages });
+                attributes: {
+                    include: [
+                        [
+                            sequelize.literal(
+                                "(SELECT COUNT(*) FROM `board_like` WHERE `board_like`.`b_id` = `board`.`b_id`)",
+                            ),
+                            "like_count",
+                        ],
+                    ],
+                },
+                order: [
+                    [sequelize.literal(orderby), "DESC"], // 카테고리가 없을시 전체리스트에서 좋아요순
+                ],
+            });
+            res.render("board/main", {
+                boardList: boardList,
+                category: category,
+                totalPages: totalPages,
+            });
+        } else {
+            let orderby = [];
+            if (like) {
+                orderby.push("like_count");
+            } else {
+                orderby.push("b_id");
+            }
+            const boardList = await board.findAll({
+                where: {
+                    category: category, // category가 있는 경우만 조회
+                },
+                limit: pageSize,
+                offset: offset,
+                include: [
+                    {
+                        model: boardLike,
+                        attributes: [],
+                    },
+                ],
+                attributes: {
+                    include: [
+                        [
+                            sequelize.literal(
+                                "(SELECT COUNT(*) FROM `board_like` WHERE `board_like`.`b_id` = `board`.`b_id`)",
+                            ),
+                            "like_count",
+                        ],
+                    ],
+                },
+                order: [
+                    [sequelize.literal(orderby), "DESC"], // like가 있을시 해당 카테고리에 좋아요순
+                ],
+            });
+            res.render("board/main", {
+                boardList: boardList,
+                category: category,
+                totalPages: totalPages,
+            });
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send("Server Error");
@@ -50,6 +108,7 @@ exports.board = async (req, res) => {
                     model: comment,
                     attributes: ["b_id", "u_id", "content"], //코멘트 조인후 값 가져오기
                 },
+                
             ],
             attributes: {
                 include: [
@@ -63,7 +122,8 @@ exports.board = async (req, res) => {
             },
         });
         console.log(boarder.comment);
-        res.json(boarder); // 뷰 생성시 값전달
+        // res.render('board/board',{board:boarder}); // 뷰 생성시 값전달
+        res.json(boarder)
     } catch (error) {
         console.error(error);
         res.status(500).send("Server Error");
@@ -71,6 +131,7 @@ exports.board = async (req, res) => {
 };
 exports.handleLike = async (req, res) => {
     try {
+        console.log(req.session);
         const { b_id, u_id } = req.body; //현재 게시글 b_id와 세션에 u_id를 받아온다
         const likeId = await boardLike.findOne({
             where: {
@@ -95,5 +156,57 @@ exports.handleLike = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send("Server Error");
+    }
+};
+
+exports.boardDelete = async (req, res) => {
+    try {
+        const b_id = Number(req.query.b_id);
+        await board.destroy({
+            where: { b_id: b_id },
+        });
+        res.end();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+};
+exports.boardInsert = async (req, res) => {
+    try {
+        const { u_id, title, content, category } = req.body;
+        const insert = await board.create({
+            u_id: u_id,
+            title: title,
+            content: content,
+            category: category,
+        });
+        if (insert) {
+            res.send("등록성공");
+        } else {
+            res.send("등록실패");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
+};
+
+exports.boardPatch = async (req, res) => {
+    console.log(req.body);
+    const { b_id, title, content, category } = req.body;
+    const update = await board.update(
+        {
+            title: title,
+            content: content,
+            category: category,
+        },
+        {
+            where: { b_id: b_id },
+        },
+    );
+    if (update) {
+        res.send("수정성공");
+    } else {
+        res.send("수정실패");
     }
 };
